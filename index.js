b34dff6f9b3fdf6059a86ef923e7caef9aff5498
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-24 14:27:29 +0300

    scope : global,functional, block and lexical scoping

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName= 'bob';
+const leastFavoriteCustomer= 'initial value';
+
+function customer(){
+return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName=customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer='not bob';
+    return bestCustomer;
+}
+
+
+function overwriteBestCustomer() {
+   bestCustomer= 'maybe bob';
+   return bestCustomer;
+}
+function changeLeastFavoriteCustomer() {
+leastFavoriteCustomer='not initial value';
+}
